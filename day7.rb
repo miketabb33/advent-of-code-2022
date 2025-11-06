14dc315334696042dd46a3153d89ccf82df7bc93
Michael Tabb <miketabb33@gmail.com>
2025-11-06 08:19:56 -0500

    solve day 7 part 1

--- day7.rb
+++ day7.rb
@@ -30,49 +30,120 @@ class Path
   end
 end
 
-def run(lines)
-  lines.each do |line|
-    if line[0] == '$'
-      symbols = line[2...].split(' ')
-      command = symbols[0] 
-      value = symbols[1]
-      yield(command, value)
-    else
-      yield('output', line)
-    end 
+class Parser
+  class << self
+    def run(lines)
+      lines.each do |line|
+        next if line[0] == '#' 
+        if line[0] == '$'
+          symbols = line[2...].split(' ')
+          command = symbols[0] 
+          value = symbols[1]
+          yield(command, value)
+        else
+          yield('output', line)
+        end 
+      end
+    end
   end
+end
+
+class DirectorySizer
+  class << self
+    def compute(dir)
+      total_size = 0
+
+      dir[:contents].each do |item|
+        if item[:kind] == :file
+          total_size += item[:size].to_i  
+        elsif item[:kind] == :dir
+          total_size += compute(item)  
+        end
+      end
+
+      dir[:total_size] = total_size  
+      total_size
+    end
+  end
+end
+
+class FileSystemBuilder
+  class << self
+    def make(lines)
+      path = Path.new
+      file_system = FileSystemBuilder.new(path: path)
+
+      Parser.run(lines) do |kind, value|
+        if kind == 'cd'
+          path.cd value
+        elsif kind == 'output'
+          data, name = value.split(' ')
+          if data == 'dir'
+            file_system.add_directory(name: name)
+          else
+            file_system.add_file(name: name, size: data)
+          end 
+        end
+      end
+
+      DirectorySizer.compute(file_system.root)
+      file_system.root
+    end
+  end
+
+  attr_reader :root
+  
+  def initialize(path:)
+    @root = { kind: :dir, name: '/', contents: [] } 
+    @path = path
+  end
+
+  def add_file(name:, size:)
+    new_file = { kind: :file, name: name, size: size }
+    current_dir[:contents] << new_file
+  end
+
+  def add_directory(name:)
+    new_dir = { kind: :dir, name: name, contents: [] }
+    current_dir[:contents] << new_dir
+  end
+
+  private 
+  
+  def current_dir
+    location = @root
+    path_list = @path.current.split('/').reject(&:empty?)
+    path_list.each do |dir_name|
+      location = location[:contents].find do |content|
+        content[:kind] == :dir && content[:name] == dir_name 
+      end
+    end
+    location    
+  end
+end
+
+
+def sum_directories(dir, threshold = 100_000)
+  sum = 0
+
+  dir[:contents].each do |item|
+    if item[:kind] == :dir
+      sum += sum_directories(item, threshold)
+      sum += item[:total_size] if item[:total_size] < threshold
+    end
+  end
+
+  sum
 end
 
 
 def part_1(lines)
-  file_system = { kind: :dir, name: '/', contents: [] }
-  path = Path.new
+  root = FileSystemBuilder.make(lines)
 
-  run(lines) do |kind, value|
-    if kind == 'cd'
-      path.cd value
-    elsif kind == 'output'
-      puts path.current
-      puts value
-      puts '------'
-    end
-  end
-
-  # puts path.current
+  puts sum_directories(root)
+  puts root
 end
 
-lines = File.read('./day7_input_sample').lines
+lines = File.read('./day7_input').lines
 
 part_1 lines
-
-# { 
-#   type: 'cd', 
-#   path: '/' 
-# },
-# { 
-#   type: 'ls', 
-#   output: [
-#     {kind: 'file', name: '', size: ''},
-#     {kind: 'dir', name: ''}
-#   ] 
-# }
